99229e9174b37cdc52756a7121708f51e10c9ae0
Jing Zhang <king91chang@163.com>
2015-09-07 09:25:53 +0800

    工程初建
    工程初建

---
+++ 仿MUZO/Controllers/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿MUZO
+//
+//  Created by wrt on 15/9/7.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 仿MUZO/Controllers/Main/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  仿MUZO
+//
+//  Created by wrt on 15/9/7.
+//  Copyright (c) 2015年 wrtsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
